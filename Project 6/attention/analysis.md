2fe938691233eb25fb8f348ce5568d49720923ed
Pedro Henrique Oliveira <phleaooliveira@gmail.com>
2025-04-03 19:09:03 -0300

    parser and attention (week 6)

---
+++ Project 6/attention/analysis.md
@@ -0,0 +1,23 @@
+# Analysis
+
+## Layer 7, Head 12
+
+Nessa attention head presente na camada 7 nós podemos perceber que a maioria dos tokens prestam atenção nos tokens que precedem eles. 
+
+Example Sentences:
+- We played [MASK] yesterday night.
+![](diagrams/1_Attention_Layer7_Head12.png)
+
+- I had [MASK] two hours ago.
+![](diagrams/2_Attention_Layer7_Head12.png)
+
+## Layer 5, Head 8
+
+Nessa attention head presente na camada 5 nós podemos perceber que o palavra após o verbo presta atenção no verbo que precede ele.
+
+Example Sentences:
+- They bought [MASK] in the mall.
+![](diagrams/1_Attention_Layer5_Head8.png)
+
+- They are waiting [MASK] to go out.
+![](diagrams/2_Attention_Layer5_Head8.png)
---
+++ Project 6/parser/parser.py
@@ -0,0 +1,94 @@
+import nltk
+import sys
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+S -> NP VP | S Conj S | S Conj VP
+NP -> N | Det N | Det Adj N | Det Adj Adj N | Det Adj Adj Adj N | NP P NP | NP Adv
+VP -> V | V NP | V NP P NP | V P NP | V Adv | V NP Adv | VP P NP
+"""
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def main():
+
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        s = input("Sentence: ")
+
+    # Convert input into list of words
+    s = preprocess(s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+    if not trees:
+        print("Could not parse sentence.")
+        return
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+
+def preprocess(sentence):
+    """
+    Convert `sentence` to a list of its words.
+    Pre-process sentence by converting all characters to lowercase
+    and removing any word that does not contain at least one alphabetic
+    character.
+    """
+    # split the words of the sentence
+    words = nltk.tokenize.word_tokenize(sentence) 
+    # check the characterers of the word and convert it to lowercase
+    words = [word.lower() for word in words if any(c.isalpha() for c in word)] 
+
+    return words
+
+
+def np_chunk(tree):
+    """
+    Return a list of all noun phrase chunks in the sentence tree.
+    A noun phrase chunk is defined as any subtree of the sentence
+    whose label is "NP" that does not itself contain any other
+    noun phrases as subtrees.
+    """
+    npChucks = []
+
+    for st in tree.subtrees():
+        if st.label() == 'NP':
+            # check if there is no NP in sts subtrees
+            if (not any(child.label() == 'NP' for child in st.subtrees(lambda t: t != st))):
+                npChucks.append(st)
+
+    return npChucks
+
+
+if __name__ == "__main__":
+    main()
